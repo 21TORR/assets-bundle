0e3c04271878f94c03a5a6f11ec607d4ca51aaee
Christian Schira <c.schira@21torr.com>
2021-01-19 10:34:30 +0100

    add cache handling for debug mode and no cache found

--- src/Storage/AssetMapCache.php
+++ src/Storage/AssetMapCache.php
@@ -2,7 +2,12 @@
 
 namespace Torr\Assets\Storage;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Torr\Assets\Command\AssetsDumpCommand;
 
 final class AssetMapCache
 {
@@ -10,12 +15,18 @@ final class AssetMapCache
 
 	private CacheItemPoolInterface $cachePool;
 
+	private KernelInterface $kernel;
+
+	private AssetDumper $assetDumper;
+
 	/**
 	 * AssetMapCache constructor.
 	 */
-	public function __construct(CacheItemPoolInterface $cachePool)
+	public function __construct(CacheItemPoolInterface $cachePool, KernelInterface $kernel, AssetDumper $assetDumper,)
 	{
 		$this->cachePool = $cachePool;
+		$this->kernel = $kernel;
+		$this->assetDumper = $assetDumper;
 	}
 
 	/**
@@ -31,7 +42,7 @@ final class AssetMapCache
 	/**
 	 * get AssetMap from Cache
 	 */
-	public function getMapFromCache() : ?AssetMap
+	public function getMapCache() : ?AssetMap
 	{
 		$cacheItem = $this->cachePool->getItem(self::CACHE_KEY);
 
@@ -43,10 +54,48 @@ final class AssetMapCache
 	}
 
 	/**
+	 * get AssetMap
+	 * in debug mode return new empty AssetMap and clear dump directory
+	 */
+	public function getAssetMap() : ?AssetMap
+	{
+		if($this->kernel->isDebug() === false) {
+			$this->assetDumper->clearDumpDirectory();
+			return new AssetMap();
+		}
+
+		$assetMap = $this->getMapCache();
+
+		// If no cache exist, execute the DumpCommand
+		if(null === $assetMap) {
+			$this->executeDumpCommand();
+			$assetMap = $this->getMapCache();
+		}
+
+		return $assetMap;
+	}
+
+	/**
 	 * clear AssetMap Cache
 	 */
 	public function clearCache() : void
 	{
 		$this->cachePool->deleteItem(self::CACHE_KEY);
+	}
+
+	/**
+	 * execute AssetsDumpCommand
+	 */
+	private function executeDumpCommand() : void
+	{
+		$application = new Application($this->kernel);
+		$application->setAutoExit(false);
+
+		$input = new ArrayInput([
+			'command' => AssetsDumpCommand::getDefaultName()
+		]);
+		$output = new NullOutput();
+
+		$application->run($input, $output);
 	}
 }
