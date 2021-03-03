3aa03d366cd57fb50d81540cec152a0e081c546f
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-03-04 00:07:22 +0100

    Heavily simplify CssUrlRewriter

--- src/File/Type/Css/CssUrlRewriter.php
+++ src/File/Type/Css/CssUrlRewriter.php
@@ -2,82 +2,51 @@
 
 namespace Torr\Assets\File\Type\Css;
 
-use function Symfony\Component\String\s;
+use Torr\Assets\Routing\AssetUrlGenerator;
 use Torr\Assets\Asset\Asset;
 use Torr\Assets\Exception\Asset\InvalidAssetException;
-use Torr\Assets\Helper\AssetsHelper;
-use Torr\Assets\Manager\AssetsManager;
 
 class CssUrlRewriter
 {
-	/** @var array<string, Asset> */
-	private array $assets = [];
+    private AssetUrlGenerator $assetUrlGenerator;
 
-	private AssetsManager $assetsManager;
-	private AssetsHelper $assetsHelper;
+    /**
+     */
+    public function __construct (AssetUrlGenerator $assetUrlGenerator)
+    {
+        $this->assetUrlGenerator = $assetUrlGenerator;
+    }
 
-	/**
-	 */
-	public function __construct (
-		AssetsManager $assetsManager,
-		AssetsHelper $assetsHelper
-	)
-	{
-		$this->assetsManager = $assetsManager;
-		$this->assetsHelper = $assetsHelper;
-	}
+    /**
+     * Find and replace namespaces in $content like url("@app/css/app.css") to url("/assets/app/css/app.css") if "@app/css/app.css" is defined
+     */
+    public function rewrite (string $content) : string
+    {
+        return \preg_replace_callback(
+            '~url\\(\\s*(?<path>.*?)\\s*\\)~i',
+            function (array $match)
+            {
+                return $this->replaceImport($match);
+            },
+            $content
+        );
+    }
 
-	/**
-	 * Find and replace namespaces in $content like url("@app/css/app.css") to url("https://example.com/assets/app/css/app.css") if "@app/css/app.css" is defined
-	 */
-	public function rewrite (string $content) : string
-	{
-		$this->findAssets($content);
-		return $this->replaceNamespace($content);
-	}
+    /**
+     * Replaces the single import with the resolved path
+     */
+    private function replaceImport(array $match) : string
+    {
+        $importPath = \trim($match["path"], " '\"");
 
-	/**
-	 * Find in $content every used namespace like url("@app/css/app.css")
-	 */
-	private function findAssets (string $content) : void
-	{
-		\preg_match_all('~url\\(\\s*(?<path>.*?)\\s*\\)~i', $content, $matches);
-
-		foreach ($matches['path'] as $item)
-		{
-			$path = s($item)->replace('"', '')->replace("'", "")->toString();
-
-			try
-			{
-				$this->assets[$path] = Asset::create($path);
-			}
-			catch (InvalidAssetException $exception)
-			{
-			}
-		}
-	}
-
-	/**
-	 * replace founded namespaces from findAssets() with path to file
-	 */
-	private function replaceNamespace (string $content) : string
-	{
-		$stringContent = s($content);
-
-		foreach ($this->assets as $key => $item)
-		{
-			$map = $this->assetsManager->getAssetMap();
-			$asset = $map->get($key);
-
-			if (null === $asset)
-			{
-				continue;
-			}
-
-			$fullPath = $this->assetsHelper->getUrl($asset->toAssetPath());
-			$stringContent = $stringContent->replace($key, $fullPath);
-		}
-
-		return $stringContent->toString();
-	}
+        try {
+            $asset = Asset::create($importPath);
+            return \sprintf('url("%s")', $this->assetUrlGenerator->getUrl($asset));
+        }
+        catch (InvalidAssetException $exception)
+        {
+            // if there is an issue -> just return unchanged
+            return $match[0];
+        }
+    }
 }
